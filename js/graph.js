70bb0b6aea4d1da396fd8804892ce6e1f38bbc48
lorentzj <lorentzj@users.noreply.github.com>
2022-01-21 21:16:02 +0000

    deploy: d3a5f23e0d294e056c55c438b80deee1f695ad4e

--- js/graph.js
+++ js/graph.js
@@ -1,7 +1,17 @@
 export function getRandomGraph() {
-    const nodeRadii = Array(10).fill(50);
+    const nodeCount = Math.floor(Math.random() * (10 - 5) + 5);
+    const nodeRadii = Array(nodeCount).fill(50);
     const nodes = nodeRadii.map((radius, index) => {
         return { radius: radius, label: `Node ${String.fromCharCode(index + 65)}`, "color": "#ff8888" };
     });
-    return { nodes: nodes, edges: [{ from: nodes[0], to: nodes[1] }] };
+    const maxEdgeCount = Math.floor(Math.random() * (20 - 7) + 7);
+    const edges = [];
+    for (let i = 0; i < maxEdgeCount; ++i) {
+        const startNode = nodes[Math.floor(Math.random() * nodes.length)];
+        const endNode = nodes[Math.floor(Math.random() * nodes.length)];
+        if (startNode !== endNode && !edges.some(e => e.from == startNode && e.to == endNode)) {
+            edges.push({ from: startNode, to: endNode });
+        }
+    }
+    return { nodes: nodes, edges: edges };
 }
